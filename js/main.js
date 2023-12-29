5e40364e55a2374ac34d1ce91b1af4cfc9596af7
djboburjon <djboburdev@gmail.com>
2023-12-29 22:39:31 +0500

    added seasonal gifs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 let hIndex = document.querySelector(".hIndex")
 let mIndex = document.querySelector(".mIndex") 
 let sIndex = document.querySelector(".sIndex") 
+let body = document.querySelector("body")
 
 
+var month = new Date().getMonth()
+
+if(month == 11 || month <= 1) {
+  body.style.backgroundImage = "url('../gifs/winter.gif')"
+}else if(month < 1 || month <= 4) {
+  body.style.backgroundImage = "url('../gifs/spring.gif')"
+}else if(month < 4 || month <= 7) {
+  body.style.backgroundImage = "url('../gifs/summer.gif')"
+}else if(month < 7 || month <= 10) {
+  body.style.backgroundImage = "url('../gifs/autumn.gif')"
+}else{
+  body.style.backgroundImage = "url('../gifs/world.gif')"
+}
 setInterval(() => {
   var date = new Date()
   var hours  = date.getHours()
